63869831d7aa3e393085d09b2920ac3efde42248
Neel Patel <np7.patel.np@gmail.com>
2021-10-02 15:30:19 +0530

    Sixth Commit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,39 @@ import logo from './logo.svg';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Content from "./components/Content";
-import './App.css';
+import PopUp from './components/PopUp';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [openPopup, setOpenPopup] = useState(false);
+  const [loggedIn, SetLoggedIn] = useState(false);
+  const [balance, setBalance] = useState(
+    parseFloat(localStorage.getItem("balance") ?? 10)
+  );
+  const [results, setResults] = useState([]);
+  useEffect(() => {
+    if (localStorage.getItem("loggedIn")) {
+      SetLoggedIn(true);
+    }
+  }, [localStorage.getItem("loggedIn")]);
+
+  useEffect(() => {
+    localStorage.setItem("balance", balance);
+  }, [balance]);
+
   return (
-    <div className="App">
-      <Header />
-      <Content />
+    <div className="casino">
+      <Header balance={balance} loggedIn={loggedIn} SetLoggedIn={SetLoggedIn} />
+      <Content setOpenPopup={setOpenPopup} results={results} setResults={setResults}/>
       <Footer />
+      {openPopup && (
+        <PopUp
+        setBalance={setBalance}
+        balance={balance}
+        setOpenPopup={setOpenPopup}
+        setResults={setResults}
+        results={results}/>
+      )}
     </div>
   );
 }
